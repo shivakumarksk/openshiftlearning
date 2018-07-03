96e7caf3799cfdc1b13de0de96efc0e92723db3e
Siva <Siva@shiva-personal>
2018-07-03 23:38:26 +0530

    Trying maven spring

---
+++ src/main/com.shiva.learn/HelloController.java
@@ -0,0 +1,19 @@
+package com.shiva.learn;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/snoop")
+public class HelloController{
+ 
+   @RequestMapping(method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+
+      return "snoop";
+   }
+
+}
