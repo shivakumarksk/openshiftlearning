647e08edc9ee83368a6091fac372fe70901b305e
Siva <Siva@shiva-personal>
2018-07-07 20:39:02 +0530

    Adding java resources

--- src/main/resources/com/shiva/spring/HelloController.java
+++ src/main/resources/com/shiva/spring/HelloController.java
@@ -1,10 +1,5 @@
 package com.shiva.learn;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.ModelMap;
-
 @Controller
 @RequestMapping("/hello")
 public class HelloController{
@@ -12,7 +7,6 @@ public class HelloController{
    @RequestMapping(method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
-
       return "hello";
    }
 
