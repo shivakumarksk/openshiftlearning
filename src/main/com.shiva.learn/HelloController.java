00554782609c59c49104c4409ba3f6180a171e8b
Siva Kumar <kshiva083@gmail.com>
2019-10-11 23:29:46 +0530

    Update HelloController.java

--- src/main/com.shiva.learn/HelloController.java
+++ src/main/com.shiva.learn/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController{
    @RequestMapping(method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework LOL!");
-//Learning Git OOOOLALALALA
+//Learning Git
       return "hello";
    }
 
